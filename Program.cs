60cebcfbb575799068b1efcb5c516bb2f739a08f
leonardo fernandes <leofernandess11@gmail.com>
2022-03-15 14:35:11 -0300

    Inicio da implementação dos inputs do usuario

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using estudos_interface.Entities;
 
 namespace estudos_interface
 {
@@ -6,7 +8,16 @@ namespace estudos_interface
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Entre com os dados do aluguel");
+            Console.WriteLine("Modelo do carro: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Retirada (dd/MM/yyyy hh:ss: ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Retornar (dd/MM/yyyy hh:ss: ");
+            DateTime finsh = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finsh, new Vehicle(model));
+
         }
     }
 }
